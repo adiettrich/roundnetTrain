1313c80b1d938ec06eb7f99b9c89e63aedbf05d0
Anton Diettrich <anton.diettrich@stud.htwk-leipzig.de>
2023-04-12 15:12:26 +0200

    added firebase and modular and fixed problems with cocoapods

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:app/app_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ModularApp(module: TrainAppModule(), child: const TrainApp()));
 }
